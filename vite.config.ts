78f4db122cafe86beba59dfc4d2c8a7b3cfd1f14
Jasiel Solís García <Jasiel>
2025-06-03 15:32:33 -0600

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/elj4so/proyecto-react.git', // Reemplaza con el nombre real del repo
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
